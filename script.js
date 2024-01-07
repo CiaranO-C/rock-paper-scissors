10bc2b603b785f9784c883bc8f409c07137e8763
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-01-07 00:31:59 +0000

    write playRound function using object key: values

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 function getComputerChoice() {
     switch(Math.floor((Math.random() * 3) + 1)) {
         case 1:
-            return "Rock";
+            return "rock";
             break;
         case 2:
-            return "Paper";
+            return "paper";
             break;
         case 3:
-            return "Scissors";
+            return "scissors";
             break
     }
 }
@@ -37,6 +37,24 @@ function getPlayerChoice() {
 
 
 
-function playRound(computerChoice, playerChoice) {
-if (playerChoice === "rock" && computerChoice === "paper") {return "You Lose! Paper beats Rock"}
+function playRound(playerChoice, computerChoice) {
+    console.log(playerChoice); //test
+    console.log(computerChoice); //test
+if (wins[playerChoice] === computerChoice) {
+    return ("You win! " + playerChoice + " beats " + computerChoice + "!");
+} else if (wins[computerChoice] === playerChoice) {
+    return ("You Loose! " + computerChoice + " beats " + playerChoice + "!");
+} else if (playerChoice === computerChoice) {
+    return "Tie! Go again!";
 }
+}
+
+
+const wins = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
+};
+
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
